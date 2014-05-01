6d5d571f37f0bf8169f547e9a33cabdf6b94d968
umbertosonnino <umbertosonnino@10.90.90.199>
2014-05-01 14:58:45 +0200

    Sistemato -AR con l'aggiunta di suffixes[]

--- src/it/uniroma1/lcl/wimmp/morphoRules/GLMorphoRuleVerbConjAR.java
+++ src/it/uniroma1/lcl/wimmp/morphoRules/GLMorphoRuleVerbConjAR.java
@@ -9,7 +9,28 @@ import java.util.List;
 import javax.naming.PartialResultException;
 
 public class GLMorphoRuleVerbConjAR extends GLMorphoRuleVerb {
+	
+	
 
+	private final String[] suffixes = { 
+			"ar", 													// 0 - infinite 
+			"ando", 												// 1 - gerund
+			"ado", "ados", 											// 2-3 - past participle masculine
+			"ada", "adas", 											// 4-5 - past participle feminine
+			"o", "as", "a", "amos", "ades", "an", 					// 6-11 - present indicative
+			"aba", "abas", "aba", "abaamos", "abades", "aban", 			//12-17 imperfect indicative
+			"ei", "aches", "ou", "amos", "astes", "aron", 			//18-23 preterite indicative
+			"ara", "aras", "ara", "aramos", "arades", "aran", 		//24-29 pluperfect indicative
+			"arei", "ar‡s", "ar‡", "aremos", "aredes", "ar‡n", 		// 30-35 future indicative 
+			"ar’a", "ar’as", "ar’a", "ariamos", "ariades", "ar’an", //36-41 conditional indicative
+			"e", "es", "e", "emos", "edes", "en", 					//42-47 present subjunctive
+			"ase", "ases", "ase", "‡semos", "‡sedes", "asen",		//48-53 preterite subjunctive 
+			"ar", "ares", "ar", "armos", "ardes", "aren",			//54-59 future subjunctive
+			"a", "e", "emos", "ade", "en",							//60-64 affirmative imperative 
+			"es", "e", "emos", "edes", "en",						//65-69 negative imperative
+			"ar", "ares", "ar", "armos", "ardes", "aren" 			//70-75 personal infinitive
+	};
+	
 	public GLMorphoRuleVerbConjAR(String title, String text) {
 		super(title, text);
 	}
@@ -18,132 +39,28 @@ public class GLMorphoRuleVerbConjAR extends GLMorphoRuleVerb {
 
 	@Override
 	public List<MorphoForm> getForms() {
-
-		/*---INFINTIE---*/
-		forms.add(new GLMorphoFormVerb(stem + "ar", "infinite", "--")); 	//1
-
-		/*---GERUND---*/
-		forms.add(new GLMorphoFormVerb(stem + "ando", "gerund", "--")); 	//2
-
-		/*---PAST PARTICIPLE---*/
+		
+		for (int i = 0; i < forms.size(); i++) {
+			((GLMorphoFormVerb) forms.get(i)).setForm(stem + suffixes[i]);
+		}
+		
+		if(!present.equals("")){ 
+			//FIRST PERSON SINGULAR PRESENT INDICATIVE IS #6
+			((GLMorphoFormVerb) forms.get(6)).setForm(present);
+		}
+		
 		if(!past.equals("")){
-			forms.add(new GLMorphoFormVerb(past, "masculine past participle" , "singular")); 	//3
-			forms.add(new GLMorphoFormVerb(past, "masculine past participle", "plural")); 
-			forms.add(new GLMorphoFormVerb(past, "feminine past participle", "singular")); 
-			forms.add(new GLMorphoFormVerb(past, "feminine past participle", "plural")); 		//6
-		}else{
-			forms.add(new GLMorphoFormVerb(stem + "ado", "masculine past participle" , "singular")); 	//3
-			forms.add(new GLMorphoFormVerb(stem + "ados", "masculine past participle", "plural")); 
-			forms.add(new GLMorphoFormVerb(stem + "ada", "feminine past participle", "singular")); 
-			forms.add(new GLMorphoFormVerb(stem + "adas", "feminine past participle", "plural")); 		//6
+			//PAST PARTICIPLE GUYS ARE #2-#5
+			((GLMorphoFormVerb) forms.get(2)).setForm(past);
+			((GLMorphoFormVerb) forms.get(3)).setForm(past);
+			((GLMorphoFormVerb) forms.get(4)).setForm(past);
+			((GLMorphoFormVerb) forms.get(5)).setForm(past);
 		}
-
-		/*---INDICATIVE---*/
-		//PRESENT
-		if(!present.equals("")){
-			forms.add(new GLMorphoFormVerb(present, "present indicative", "first singular")); 	//7
-		}else {
-			forms.add(new GLMorphoFormVerb(stem + "o", "present indicative", "first singular")); 	//7
+		
+		if(!preterite.equals("")){
+			//FIRST PERSON SINGULAR PRETERITE INDICATIVE IS #18
+			((GLMorphoFormVerb) forms.get(18)).setForm(preterite);
 		}
-		forms.add(new GLMorphoFormVerb(stem + "as", "present indicative", "second singular"));
-		forms.add(new GLMorphoFormVerb(stem + "a", "present indicative", "third singular"));
-		forms.add(new GLMorphoFormVerb(stem + "amos", "present indicative", "first plural"));
-		forms.add(new GLMorphoFormVerb(stem + "ades", "present indicative", "second plural"));
-		forms.add(new GLMorphoFormVerb(stem + "an", "present indicative", "third plural")); 	//12
-
-		//IMPERFECT
-		forms.add(new GLMorphoFormVerb(stem + "aba", "imperfect indicative", "first singular"));	//13
-		forms.add(new GLMorphoFormVerb(stem + "abas", "imperfect indicative", "second singular"));
-		forms.add(new GLMorphoFormVerb(stem + "aba", "imperfect indicative", "third singular"));
-		forms.add(new GLMorphoFormVerb(stem + "abamos", "imperfect indicative", "first plural"));
-		forms.add(new GLMorphoFormVerb(stem + "abades", "imperfect indicative", "second plural"));
-		forms.add(new GLMorphoFormVerb(stem + "aban", "imperfect indicative", "third plural")); //18
-
-		//PRETERITE
-		if(! preterite.equals("")){
-			forms.add(new GLMorphoFormVerb(preterite, "preterite indicative", "first singular")); 	//19
-		}else{
-			forms.add(new GLMorphoFormVerb(stem + "ei", "preterite indicative", "first singular")); 	//19
-		}
-		forms.add(new GLMorphoFormVerb(stem + "aches", "preterite indicative", "second singular"));
-		forms.add(new GLMorphoFormVerb(stem + "ou", "preterite indicative", "third singular"));
-		forms.add(new GLMorphoFormVerb(stem + "amos", "preterite indicative", "first plural"));
-		forms.add(new GLMorphoFormVerb(stem + "astes", "preterite indicative", "second plural"));
-		forms.add(new GLMorphoFormVerb(stem + "aron", "preterite indicative", "third plural"));		//24
-
-		//PLUPERFECT
-		forms.add(new GLMorphoFormVerb(stem + "ara", "pluperfect indicative", "first singular"));	//25
-		forms.add(new GLMorphoFormVerb(stem + "aras", "pluperfect indicative", "second singular"));
-		forms.add(new GLMorphoFormVerb(stem + "ara", "pluperfect indicative", "third singular"));
-		forms.add(new GLMorphoFormVerb(stem + "aramos", "pluperfect indicative", "first plural"));
-		forms.add(new GLMorphoFormVerb(stem + "arades", "pluperfect indicative", "second plural"));
-		forms.add(new GLMorphoFormVerb(stem + "aran", "pluperfect indicative", "third plural"));	//30
-
-		//FUTURE
-		forms.add(new GLMorphoFormVerb(stem + "arei", "future indicative", "first singular"));		//31
-		forms.add(new GLMorphoFormVerb(stem + "ar‡s", "future indicative", "second singular"));
-		forms.add(new GLMorphoFormVerb(stem + "ar‡", "future indicative", "third singular"));
-		forms.add(new GLMorphoFormVerb(stem + "aremos", "future indicative", "first plural"));
-		forms.add(new GLMorphoFormVerb(stem + "aredes", "future indicative", "second plural"));
-		forms.add(new GLMorphoFormVerb(stem + "ar‡n", "future indicative", "third plural"));		//36
-
-		//CONDITIONAL
-		forms.add(new GLMorphoFormVerb(stem + "ar’a", "conditional indicative", "first singular")); //37
-		forms.add(new GLMorphoFormVerb(stem + "ar’as", "conditional indicative", "second singular"));
-		forms.add(new GLMorphoFormVerb(stem + "ar’a", "conditional indicative", "third singular"));
-		forms.add(new GLMorphoFormVerb(stem + "ariamos", "conditional indicative", "first plural"));
-		forms.add(new GLMorphoFormVerb(stem + "ariades", "conditional indicative", "second plural"));
-		forms.add(new GLMorphoFormVerb(stem + "ar’an", "conditional indicative", "third plural"));	//42
-
-		/*---SUBJUNCTIVE---*/
-		//PRESENT
-		forms.add(new GLMorphoFormVerb(stem + "e", "present subjunctive", "first singular"));		//43
-		forms.add(new GLMorphoFormVerb(stem + "es", "present subjunctive", "second singular"));
-		forms.add(new GLMorphoFormVerb(stem + "e", "present subjunctive", "third singular"));
-		forms.add(new GLMorphoFormVerb(stem + "emos", "present subjunctive", "first plural"));
-		forms.add(new GLMorphoFormVerb(stem + "edes", "present subjunctive", "second plural"));
-		forms.add(new GLMorphoFormVerb(stem + "en", "present subjunctive", "third plural"));		//48
-
-		//PRETERITE
-		forms.add(new GLMorphoFormVerb(stem + "ase", "subjunctive preterite", "first singular"));	//49
-		forms.add(new GLMorphoFormVerb(stem + "ases", "subjunctive preterite", "second singular"));
-		forms.add(new GLMorphoFormVerb(stem + "ase", "subjunctive preterite", "third singular"));
-		forms.add(new GLMorphoFormVerb(stem + "‡semos", "subjunctive preterite", "first plural"));
-		forms.add(new GLMorphoFormVerb(stem + "‡sedes", "subjunctive preterite", "second plural"));
-		forms.add(new GLMorphoFormVerb(stem + "asen", "subjunctive preterite", "third plural"));	//54
-
-		//FUTURE
-		forms.add(new GLMorphoFormVerb(stem + "ar", "subjunctive future", "first singular"));		//55
-		forms.add(new GLMorphoFormVerb(stem + "ares", "subjunctive future", "second singular"));
-		forms.add(new GLMorphoFormVerb(stem + "ar", "subjunctive future", "third singular"));
-		forms.add(new GLMorphoFormVerb(stem + "armos", "subjunctive future", "first plural"));
-		forms.add(new GLMorphoFormVerb(stem + "ardes", "subjunctive future", "second plural"));
-		forms.add(new GLMorphoFormVerb(stem + "aren", "subjunctive future", "third plural"));		//60
-
-		/*---IMPERATIVE---*/
-		//AFFIRMATIVE
-		forms.add(new GLMorphoFormVerb(stem + "a", "imperative affirmative", "second singular"));	//61
-		forms.add(new GLMorphoFormVerb(stem + "e", "imperative affirmative", "third singular"));
-		forms.add(new GLMorphoFormVerb(stem + "emos", "imperative affirmative", "first plural"));
-		forms.add(new GLMorphoFormVerb(stem + "ade", "imperative affirmative", "second plural"));
-		forms.add(new GLMorphoFormVerb(stem + "en", "imperative affirmative", "third plural"));		//65
-
-		//NEGATIVE
-		forms.add(new GLMorphoFormVerb(stem + "es", "imperative negative", "second singular"));		//66
-		forms.add(new GLMorphoFormVerb(stem + "e", "imperative negative", "third singular"));
-		forms.add(new GLMorphoFormVerb(stem + "emos", "imperative negative", "first plural"));
-		forms.add(new GLMorphoFormVerb(stem + "edes", "imperative negative", "second plural"));
-		forms.add(new GLMorphoFormVerb(stem + "en", "imperative negative", "third plural"));		//70
-
-		/*---PERSONAL INFINITIVE---*/
-		forms.add(new GLMorphoFormVerb(stem + "ar", "personal infinitive", "first singular"));		//71
-		forms.add(new GLMorphoFormVerb(stem + "ares", "personal infinitive", "first singular"));
-		forms.add(new GLMorphoFormVerb(stem + "ar", "personal infinitive", "third singular"));
-		forms.add(new GLMorphoFormVerb(stem + "armos", "personal infinitive", "first plural"));
-		forms.add(new GLMorphoFormVerb(stem + "ardes", "personal infinitive", "second plural"));
-		forms.add(new GLMorphoFormVerb(stem + "aren", "personal infinitive", "third plural"));		//76
-
-
 
 		return forms;
 	}
