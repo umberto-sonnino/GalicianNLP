54b637b1a79d7f33986c1eace6b5c0421ac2986f
Scafooo <scafoglierifederico@gmail.com>
2014-05-04 01:03:21 +0200

    ProNoun e ProperNoun complete

--- src/it/uniroma1/lcl/wimmp/morphoRules/GalicianMorphoRuleProperNoun.java
+++ src/it/uniroma1/lcl/wimmp/morphoRules/GalicianMorphoRuleProperNoun.java
@@ -3,6 +3,7 @@
  */
 package it.uniroma1.lcl.wimmp.morphoRules;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import it.uniroma1.lcl.wimmp.MorphoForm;
@@ -13,6 +14,14 @@ import it.uniroma1.lcl.wimmp.MorphoRule;
  *
  */
 public class GalicianMorphoRuleProperNoun implements MorphoRule {
+	
+	String title;
+	String text;
+	
+	public GalicianMorphoRuleProperNoun(String title,String text){
+		this.title=title;
+		this.text=text;
+	}
 
 	/* (non-Javadoc)
 	 * @see it.uniroma1.lcl.wimmp.MorphoRule#getForms()
@@ -20,7 +29,15 @@ public class GalicianMorphoRuleProperNoun implements MorphoRule {
 	@Override
 	public List<MorphoForm> getForms() {
 		// TODO Auto-generated method stub
-		return null;
+		ArrayList<MorphoForm> al=new ArrayList<MorphoForm>();
+		
+		if(text.contains("|f"))
+			al.add(new MorphoForm(this.title,"feminile"));
+		if(text.contains("|m"))
+			al.add(new MorphoForm(this.title,"masculine"));
+		
+		
+		return al;
 	}
 
 }
